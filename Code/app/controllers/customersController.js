27dbbab6ccccc2e7ee6c531c2dba0ce423f52476
GuilhermeFontanella <gui.jmf@hotmail.com>
2023-06-06 12:06:03 -0300

    animations

--- Code/app/controllers/customersController.js
+++ Code/app/controllers/customersController.js
@@ -15,14 +15,35 @@
            $scope.reverse = !$scope.reverse;
         };
 
-        customersService.getCustomers()
-            .success(function (customers) {
-                $scope.customers = customers;
-            })
-            .error(function (data, status, headers, config) {
-                // handle errors
-            })
-        
+        init = function() {
+            console.log('iniciou');
+            $scope.getCustomers();
+        }
+
+        $scope.getCustomers = function() {
+            customersService.getCustomers()
+                .success(function (customers) {
+                    $scope.customers = customers;
+                })
+                .error(function (data, status) {
+                    // handle errors
+                });
+        };
+
+        $scope.deteleCustomer = function(idCustomer) {
+            console.log(idCustomer);
+            customersService.deleteCustomer(idCustomer)
+                .success(function () {
+                    window.alert('Cliente excluído com sucesso!');
+                    $scope.getCustomers();
+                })
+                .error(function (data, status) {
+                    console.error(`Erro: ${data} - Status: ${status}`);
+                    window.alert('Ocorreu um erro ao excluir este cliente.');
+                });
+        }
+
+        init();
         
     };
     
--- Code/app/services/customersService.js
+++ Code/app/services/customersService.js
@@ -5,6 +5,23 @@
             this.getCustomers = function () {
                 return $http.get('/customers');
             };
+
+            this.deleteCustomer = function (customerId) {
+                console.log('veio daqui');
+                return $http.delete(`/customers/${customerId}`);
+            };
+
+            this.editCustomer = function (customerId, data) {
+                return $http.put(`/customers/${customerId}`, data);
+            };
+
+            this.getCustomerById = function (customerId) {
+                return $http.get(`/customers/${customerId}`);
+            };
+
+            this.createCustomer = function (data) {
+                return $http.post(`/customers/`, data);
+            };
         };
 
         angular.module('customersApp')
